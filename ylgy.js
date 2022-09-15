c126aa9a65c25dc99431d95c256ee6eb86a1b30b
Heyboi <38832550+dandnaloveU@users.noreply.github.com>
2022-09-16 00:32:22 +0800

    Update ylgy.js

--- ylgy.js
+++ ylgy.js
@@ -3,7 +3,7 @@ var url = $request.url
 
 if (body) {
   var obj = JSON.parse($response.body)
-  obj.data.map_data ='{"4":1}'
+  obj.data.map_data.blockTypeData ='{"4":1}'
   $done({ body: JSON.stringify(obj) })
 } else {
   $done({})
